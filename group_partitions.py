ee51221bac27ace69df37c23bf8534ca3280abd4
scimaths <mevaibhav045@gmail.com>
2023-02-27 01:54:06 +0530

    Updated experimentation to edges, updated logs

--- group_partitions.py
+++ group_partitions.py
@@ -1,5 +1,6 @@
 import igraph as ig
 import numpy as np
+import copy
 import random
 import argparse
 
@@ -7,7 +8,7 @@ parser = argparse.ArgumentParser(prog='Simulating group partitioning')
 parser.add_argument('--num_graphs', type=int)
 parser.add_argument('--nodes', type=int)
 parser.add_argument('--seed', type=int, default=0)
-parser.add_argument('--prob', type=float, default=0.7)
+# parser.add_argument('--prob', type=float, default=0.7)
 
 def set_seed(seed=0):
     np.random.seed(seed)
@@ -23,7 +24,7 @@ def generate_2_allocations(num: int):
             allocation = np.zeros((2, num))
             allocation[:, indices] = 1
             allocation[1, :] = 1-allocation[1, :]
-            yield allocation
+            yield allocation.astype(np.int32)
 
 def test_generate_2_allocations():
     for num in range(2, 21):
@@ -39,12 +40,13 @@ def utilities(g: ig.Graph, allocation: np.ndarray):
 
     utilities = graph_adj @ allocation.T
     cross_utilities = utilities @ allocation - graph_adj
+    co_allocation = allocation.T @ allocation
     # cross_allocation = (allocation.T @ allocation).astype(np.bool)
     # cross_utilities[cross_allocation == True] = -1
-    envy = np.maximum(0, cross_utilities - np.diag(cross_utilities).reshape(-1, 1))
+    envy = np.maximum(0, cross_utilities - np.diag(cross_utilities).reshape(-1, 1)) * (1 - co_allocation)
     return {
-        'cross_utilities': cross_utilities,
-        'envy': envy
+        'cross_utilities': cross_utilities.astype(np.int32),
+        'envy': envy.astype(np.int32)
     }
 
 def test_utilities():
@@ -59,37 +61,44 @@ def test_utilities():
 
 def get_min_envy_2_allocation(g: ig.Graph):
     n = len(g.get_adjacency().data)
-    min_envy = n
+    min_envy = n-1
     best_alloc = None
+    swap_tuple = None
     for alloc in generate_2_allocations(n):
         util = utilities(g, alloc)
         if min_envy > np.max(util['envy']):
-            best_alloc = alloc
+            best_alloc = copy.deepcopy(alloc)
             min_envy = int(np.max(util['envy']))
+            swap_tuple = np.unravel_index(np.argmax(util['envy']), (n, n))
     return {
         'min_envy': min_envy,
-        'swap_tuple': np.unravel_index(np.argmax(util['envy']), (n, n)),
+        'swap_tuple': swap_tuple,
         'best_alloc': best_alloc
     }
 
-def test_ef_max(n, num_graphs=1000, seed=0, prob=0.7):
+# def test_ef_max(n, num_graphs=1000, seed=0, prob=0.7):
+def test_ef_max(n, num_graphs=1000, seed=0):
     set_seed(seed)
     filename = f"logs/log_{n}_{num_graphs}.txt"
     with open(filename, 'w') as f:
-        f.write(f"Experiment with {num_graphs} graphs, number of nodes = {n}, seed = {seed}, edge-prob = {prob}\n\n")
-    max_envy = 0
-    for _ in range(num_graphs):
-        g = ig.Graph.Erdos_Renyi(n=n, p=prob)
-        result = get_min_envy_2_allocation(g)
-        if result['min_envy'] > max_envy:
-            max_envy = result['min_envy']
-            with open(filename, 'a') as f:
-                f.write(f"Envy of {max_envy} found, swap {result['swap_tuple']}\n")
-                f.write(f"{'-'*12}GRAPH{'-'*13}\n" + str(np.array(g.get_adjacency().data)) + "\n")
-                f.write(f"{'-'*10}ALLOCATION{'-'*10}\n" + str(result['best_alloc']) + "\n\n")
+        f.write(f"Experiment with {num_graphs} graphs, number of nodes = {n}, seed = {seed}\n\n")
+    for m in range(n, (n*(n-1))//2 + 1):
+        with open(filename, 'a') as f:
+            f.write(f"{'-'*20}{m} edges{'-'*20}\n\n")
+        max_envy = 0
+        for _ in range(num_graphs):
+            g = ig.Graph.Erdos_Renyi(n=n, m=m)
+            result = get_min_envy_2_allocation(g)
+            if result['min_envy'] > max_envy:
+                max_envy = result['min_envy']
+                with open(filename, 'a') as f:
+                    f.write(f"Envy of {max_envy} found, swap {result['swap_tuple']}\n")
+                    f.write(f"{'-'*12}GRAPH{'-'*13}\n" + str(np.array(g.get_adjacency().data)) + "\n")
+                    f.write(f"{'-'*10}ALLOCATION{'-'*10}\n" + str(result['best_alloc']) + "\n\n")
     with open(filename, 'a') as f:
         f.write(f"Experiment complete")
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    test_ef_max(args.nodes, args.num_graphs, args.seed, args.prob)
+    # test_ef_max(args.nodes, args.num_graphs, args.seed, args.prob)
+    test_ef_max(args.nodes, args.num_graphs, args.seed)
